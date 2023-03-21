bc65a8be61f78f65eab348c9a2bc1fac92e6e9fe
joannamickamedina <90305331+joannamickamedina@users.noreply.github.com>
2023-03-21 12:26:55 +0800

    Add files via upload

---
+++ Demo 1.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE testdatabase;
+personsCREATE TABLE PersonsTable(
+CREATE TABLE orderstable(OrderID INT NOT NULL, OrderNumber INT NOT NULL, PRIMARY KEY (OrderID));
+INSERT INTO orderstable(OrderID, OrderNumber) VALUES('1','77895');
+INSERT INTO orderstable(OrderID, OrderNumber) VALUES('2','44678');
+INSERT INTO orderstable(OrderID, OrderNumber) VALUES('3','22456');
