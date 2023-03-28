7ef4de8b680685ec4d8b3ff6fb1c91a036ca32da
joannamickamedina <90305331+joannamickamedina@users.noreply.github.com>
2023-03-28 13:04:14 +0800

    Add files via upload

---
+++ Demo 2.sql
@@ -0,0 +1,5 @@
+UPDATE personstable SET LastName = 'Santos' WHERE PersonsID = 3;
+Select *FROM personstable;
+UPDATE personstable SET FirstName = 'Joanna Micka' WHERE PersonsID = 4;
+UPDATE personstable SET Age = '20' WHERE PersonsID = 4;
+DELETE FROM personstable WHERE PersonsID = 1;
